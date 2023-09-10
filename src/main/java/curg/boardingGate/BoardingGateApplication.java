a683411a1b2be3af934b68b0a97eb3c21a140734
Chris <hereokay88@gmail.com>
2023-09-10 16:04:17 +0900

    Chore: 스프링 초기 세팅

---
+++ src/main/java/curg/boardingGate/BoardingGateApplication.java
@@ -0,0 +1,13 @@
+package curg.boardingGate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardingGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/curg/boardingGate/BoardingGateApplicationTests.java
@@ -0,0 +1,13 @@
+package curg.boardingGate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
