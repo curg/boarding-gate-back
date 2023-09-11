429d4d746daacfed9988478fc77f4a95c309cec4
hereokay <hereokay88@gmail.com>
2023-09-11 17:35:17 +0900

    init

--- src/main/java/curg/boardingGate/BoardingGateApplication.java
+++ src/main/java/curg/boardingGate/BoardingGateApplication.java
@@ -2,12 +2,16 @@ package curg.boardingGate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@RestController
 public class BoardingGateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardingGateApplication.class, args);
 	}
-
 }
